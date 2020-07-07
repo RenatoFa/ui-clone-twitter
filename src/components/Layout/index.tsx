aacc6a87b01a3b37324a0b156b1e0f75d0efed5c
Renato Ferreira de Assis <renato.v6@hotmail.com>
2020-07-07 19:46:55 -0300

    Estilização  do Menu

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Layout: React.FC = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Menubar>
+
+                </Menubar>
+
+            </Wrapper>
+
+        </Container>
+    );
+}
+
+export default Layout;
