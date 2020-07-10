ada2b3ca14e62394bb89fa214e46003033b84ed8
Renato Ferreira de Assis <renato.v6@hotmail.com>
2020-07-10 11:57:33 -0300

    Estilização do ProfilePage

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {Container,Banner , Avatar , ProfileData,LocationIcon,CakeIcon,Followage} from './styles'
+
 
 const ProfilePage: React.FC =()=>{
     return (
@@ -9,12 +11,36 @@ const ProfilePage: React.FC =()=>{
 
             </Banner>
             <ProfileData>
-                <EditButton outline >Editar Perfil</EditButton>
+               { //<EditButton outline >Editar Perfil</EditButton>
+               }
                 <h1>Renato Ferreira de Assis</h1>
                 <h2>Renato_Fa</h2>
                 <p>
                     Developer at Renato
                 </p>
+                <ul>
+                    <li>
+                        <LocationIcon/>
+                        Rio de Janeiro , Brasil
+
+                    </li>
+                    <li>
+                        <CakeIcon/>
+                        Nascido(a) em 27 de Março de 2020
+
+                    </li>
+                </ul>
+
+                <Followage>
+                    <span>
+                        seguindo <strong>243</strong>
+
+                    </span>
+                    <span>
+                       <strong>263 </strong> seguidores
+
+                    </span>
+                </Followage>
             </ProfileData>
 
         </Container>
--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -3,3 +3,32 @@ import styled from 'styled-components';
 export const Container = styled.div`
   
 `;
+
+export const Banner = styled.div`
+  
+`;
+
+export const Avatar= styled.div`
+  
+`;
+
+export const ProfileData= styled.div`
+  
+`;
+
+
+
+
+
+export const LocationIcon= styled.div`
+  
+`;
+
+export const CakeIcon= styled.div`
+  
+`;
+
+export const Followage= styled.div`
+  
+`;
+
