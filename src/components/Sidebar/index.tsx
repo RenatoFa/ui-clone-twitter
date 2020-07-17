1d56b65df59d5facbc5f181b42cafc08fef35061
Renato Ferreira de Assis <renato.v6@hotmail.com>
2020-07-17 20:56:23 -0300

    Fim do Html e Começo da estilização da SideBar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import {Container,
+SearchWrapper,
+SearchInput,
+SearchIcon,
+Body} from './styles'
 
 
 const Sidebar: React.FC = () => {
@@ -7,9 +11,15 @@ const Sidebar: React.FC = () => {
       <Container>
           <SearchWrapper>
               <SearchInput placeholder='Busca no Twitter' />
-              
+              <SearchIcon/>
           </SearchWrapper>
+          <Body>
+
+              <p> {'Lorem ipsum dolor sit amer. ' .repeat(90)} </p>
+
+          </Body>
       </Container>
+
   );
 }
 
