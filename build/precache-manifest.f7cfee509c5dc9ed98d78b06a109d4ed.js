4591f5214def565bff576fecd7dc92a5ea5ead72
Renato Ferreira de Assis <renato.v6@hotmail.com>
2020-07-19 08:01:12 -0300

    Build

---
+++ build/precache-manifest.f7cfee509c5dc9ed98d78b06a109d4ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71141e783050d3e0f854faea9f7641f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c66bbfde7b6b4e32fd3e",
+    "url": "/static/js/2.8b929ec9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8b929ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b938ec9031ee3a23974",
+    "url": "/static/js/main.220bb337.chunk.js"
+  },
+  {
+    "revision": "3d3a699bf5d7ac5e0ddf",
+    "url": "/static/js/runtime-main.7c37ce58.js"
+  }
+]);
